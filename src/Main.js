bcf87e1212bda270971f96c571967767b194e0c2
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-10-24 15:34:58 +0000

    fetching with async await

--- src/Main.js
+++ src/Main.js
@@ -6,27 +6,38 @@ const Main = (props) => {
 
     const fetchData_Then = () => {
         
-        let myData = [];
+        //let myData = [];
 
-        console.log('starting fetch..');
+        console.log('starting then fetch..');
 
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then((response) => {
                 console.log(response);
-                myData = response.data;
-                console.log(myData) // Burada yazarsak array i goruruz
+                //myData = response.data;
+                //console.log(myData) // Burada yazarsak array i goruruz
             })
 
-        console.log(myData) // Burada bos bir array aliriz cunku henuz response tamamlanmamistir    
-        console.log('end fetch..');
+        //console.log(myData) // Burada bos bir array aliriz cunku henuz response tamamlanmamistir    
+        console.log('end then fetch..');
+    }
+
+    const fetchData_Await = async () => {
+        console.log('starting await fetch..')
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        console.log(response);
+        console.log('end await fetch..')
     }
     return (
         <SafeAreaView>
             <View>
                 <Text>Main</Text>
                 <Button
-                    title='Fetch Data'
+                    title='Fetch Data Then'
                     onPress={fetchData_Then}
+                />
+                <Button
+                    title='Fetch Data Await'
+                    onPress={fetchData_Await}
                 />
             </View>
 
