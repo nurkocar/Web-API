71751a9ebf240c5c4197f30c8277674aa405e0b7
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-10-24 15:26:37 +0000

    Fetching with then-catch

---
+++ src/Main.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React from 'react';
+import { SafeAreaView, View, Text, Button } from 'react-native';
+
+const Main = (props) => {
+
+    const fetchData_Then = () => {
+        
+        let myData = [];
+
+        console.log('starting fetch..');
+
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((response) => {
+                console.log(response);
+                myData = response.data;
+                console.log(myData) // Burada yazarsak array i goruruz
+            })
+
+        console.log(myData) // Burada bos bir array aliriz cunku henuz response tamamlanmamistir    
+        console.log('end fetch..');
+    }
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Main</Text>
+                <Button
+                    title='Fetch Data'
+                    onPress={fetchData_Then}
+                />
+            </View>
+
+        </SafeAreaView>
+    );
+}
+
+export default Main;
