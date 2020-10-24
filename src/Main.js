c461aaae5a5156f051351198fd153a11392686bc
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-10-24 16:17:34 +0000

    fetching real data with then-catch and async-await

--- src/Main.js
+++ src/Main.js
@@ -1,36 +1,29 @@
 import axios from 'axios';
-import React from 'react';
-import { SafeAreaView, View, Text, Button } from 'react-native';
+import React, {useState} from 'react';
+import { SafeAreaView, View, Text, Button, FlatList } from 'react-native';
 
 const Main = (props) => {
 
+    const [userData, setUserData] = useState([]);
+
     const fetchData_Then = () => {
-        
-        //let myData = [];
-
-        console.log('starting then fetch..');
-
         axios.get('https://jsonplaceholder.typicode.com/users')
-            .then((response) => {
-                console.log(response);
-                //myData = response.data;
-                //console.log(myData) // Burada yazarsak array i goruruz
-            })
-
-        //console.log(myData) // Burada bos bir array aliriz cunku henuz response tamamlanmamistir    
-        console.log('end then fetch..');
+        .then(({data}) => {
+            console.log(data);
+            setUserData(data);
+        })
     }
 
     const fetchData_Await = async () => {
-        console.log('starting await fetch..')
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-        console.log(response);
-        console.log('end await fetch..')
+        const {data} = await axios.get('https://jsonplaceholder.typicode.com/users')
+        setUserData(data);
     }
+
+    
     return (
         <SafeAreaView>
             <View>
-                <Text>Main</Text>
+
                 <Button
                     title='Fetch Data Then'
                     onPress={fetchData_Then}
@@ -38,6 +31,12 @@ const Main = (props) => {
                 <Button
                     title='Fetch Data Await'
                     onPress={fetchData_Await}
+                />
+
+                <FlatList
+                    data = {userData}
+            renderItem = {({item}) => <Text>{item.name}</Text>}
+                
                 />
             </View>
 
